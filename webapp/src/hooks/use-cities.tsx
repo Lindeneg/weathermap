50d0ecae46fe754cb8e853dc4682fed01189ae82
lindeneg <christian@lindeneg.org>
2024-10-17 22:50:23 +0200

    implement use-cities hook
    initial crack at it, would like to cache the response from weather api for 3600 seconds but I'll push that as a would-be-nice task for later

---
+++ webapp/src/hooks/use-cities.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import constants from "@/constants";
+import { useFetch } from "./use-fetch";
+
+type WeatherResponse = {
+    weather: { main: string; description: string; icon: string }[];
+    main: Record<
+        "temp" | "feels_like" | "temp_min" | "temp_max" | "humidity",
+        number
+    >;
+    wind: Record<"speed", number>;
+    clouds: Record<"all", number>;
+};
+
+export type City = {
+    id: number;
+    name: string;
+    display: string;
+    country: string;
+    lat: number;
+    lng: number;
+    population: number | null;
+    weather?: WeatherResponse | null;
+};
+
+export type UseCitiesHook = ReturnType<typeof useCities>;
+
+export const useCities = () => {
+    const [suggestions, setSuggestions] = useState<City[]>([]);
+    const [selected, setSelected] = useState<City | null>(null);
+    const [query, setQuery] = useState("");
+    const { get } = useFetch();
+
+    useEffect(() => {
+        (async () => {
+            const response = await get<City[]>(
+                `${constants.SERVER_URL}/cities/?q=${query}&limit=5&offset=0`
+            );
+            setSuggestions(response ?? []);
+        })();
+    }, [query, get]);
+
+    return {
+        suggestions,
+        selected,
+        query,
+        setQuery,
+        setSuggestions,
+        setSelected: async (city: City | null) => {
+            // TODO: could cache this result with 3600s TTL
+            if (city) {
+                const response = await get<WeatherResponse>(
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lng}&exclude=minutely,hourly,daily,alerts&units=metric&appid=${constants.OPENWEATHER_KEY}`
+                );
+                city.weather = response
+                    ? {
+                          weather: response.weather,
+                          main: response.main,
+                          wind: response.wind,
+                          clouds: response.clouds,
+                      }
+                    : null;
+            }
+            setSelected(city);
+        },
+    };
+};
